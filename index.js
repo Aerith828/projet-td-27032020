abbca0e32fcd4cd039f715a9e8e570b982a2189b
Aerith828 <58862394+Aerith828@users.noreply.github.com>
2020-03-27 12:17:53 +0100

    create delete-article.html and update js file and readme

--- index.js
+++ index.js
@@ -36,8 +36,28 @@ app.route("/api/articles/create")
         const sqlConnection = mysql.createConnection(sqlConfig);
 
         sqlConnection.query(
-            "INSERT INTO node_articles VALUES (NULL, ?, ?, ?, ?, ?)", 
+            "INSERT INTO node_articles VALUES (NULL, ?, ?, ?, ?)", 
             [req.body.title, req.body.content, req.body.author, req.body.created_at],(error, result) => {
+                if (error) {
+                    console.log("ERROR :", error.code);
+                    res.status(503).send({ status: "ERROR" });
+                } else {
+                    console.log(result);
+                    res.send({ status : "OK" });
+                }
+                sqlConnection.end();
+            });
+    });
+
+app.route("/api/articles/delete")
+    .get((req, res) => res.status(503).send({ status: "ERROR"}))
+    .post((req, res) => {
+        const sqlConnection = mysql.createConnection(sqlConfig);
+
+        sqlConnection.query(
+            "DELETE FROM node_articles WHERE id = ?",
+            [ req.body.id ],
+            (error, result) => {
                 if (error) {
                     console.log("ERROR :", error.code);
                     res.status(503).send({ status: "ERROR" });
